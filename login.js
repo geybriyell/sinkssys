592cb64f28796c3d430a411b65b6e363ec41b2ec
Brenzer Ace <brenzeracedulnuan@gmail.com>
2024-10-13 01:26:17 +0800

    Revert "aaaa"

--- login.js
+++ login.js
@@ -1,19 +1,1 @@
-function openModal(name) {
-    const employee = employees[name];
-    document.getElementById('employee-name').innerText = name;
-    document.getElementById('employee-job-title').innerText = employee.jobTitle;
-    document.getElementById('employee-department').innerText = employee.department;
-    document.getElementById('employee-email').innerText = employee.email;
-    document.getElementById('employee-contact').innerText = employee.contact;
-    document.getElementById('employee-address').innerText = employee.address;
-    document.getElementById('employee-sex').innerText = employee.sex;
-    document.getElementById('employee-status').innerText = employee.status;
-    document.getElementById('employee-modal').classList.remove('hidden');
-  }
-  
-  function closeModal() {
-    document.getElementById('employee-modal').classList.add('hidden');
-  }
-  
-
 
